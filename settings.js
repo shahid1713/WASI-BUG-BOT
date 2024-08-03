e533b5c258f6a8631d12d3f0e96d4666bb993014
shahid1713 <malik.shahid.1713@gmail.com>
2024-08-03 12:02:12 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923287879369"
+global.ownername = "Hacker "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownernumber = '923287879369'  //creator number
+global.ownername = ' Hacker ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
